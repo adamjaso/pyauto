92877d5a17dff2be489740db577777a95c948b42
Adam Jaso <ajaso@hsdp.io>
2018-01-19 18:41:00 -0800

    trying to fix broken pyauto core install

--- core/setup.py
+++ core/setup.py
@@ -1,5 +1,5 @@
 from os import path
-from setuptools import setup
+from setuptools import setup, find_packages
 from pyauto.core import __version__
 
 
@@ -7,18 +7,17 @@ dirname = path.realpath(path.dirname(__file__))
 with open(path.join(dirname, 'requirements.txt')) as f:
     reqs = [l for l in f.read().strip().split('\n') if not l.startswith('-')]
 
-license = path.join(dirname, 'LICENSE')
+license = path.join(dirname, '../LICENSE')
 readme = path.join(dirname, 'README.md')
 
 setup(
-    name='pyauto',
+    name='pyauto.core',
     author='Adam Jaso',
     version=__version__,
     description='PyAuto Core',
     license=open(license).read(),
     long_description=open(readme).read(),
-    packages=['pyauto.core'],
-    package_dir={'pyauto.core': 'pyauto/core'},
+    packages=find_packages(),
     install_requires=reqs,
     url='https://github.com/adamjaso/pyauto',
 )
