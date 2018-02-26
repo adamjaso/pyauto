40aee4d55c0ba7233b01baaeb25fb05dd5f5a497
Adam Jaso <ajaso@hsdp.io>
2018-02-26 07:51:50 -0800

    renamed arg of j2util render in util

--- util/pyauto/util/j2util.py
+++ util/pyauto/util/j2util.py
@@ -30,12 +30,12 @@ def build_env(dirname):
 def get_template_renderer(templates_dirname):
     env = build_env(templates_dirname)
 
-    def _render(filename, **kwargs):
+    def _render(filename_, **kwargs):
         cwd = os.getcwd()
-        if filename.startswith(templates_dirname):
-            filename = filename.replace(templates_dirname, '')
-        filename = re.sub('^/', '', filename)
+        if filename_.startswith(templates_dirname):
+            filename_ = filename_.replace(templates_dirname, '')
+        filename_ = re.sub('^/', '', filename_)
         kwargs['CURRENT_DIRECTORY'] = cwd
-        return env.get_template(filename).render(**kwargs)
+        return env.get_template(filename_).render(**kwargs)
 
     return _render
